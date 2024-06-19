71c0afaa9dc1f3dd32f356609d75be66230d2da7
Carlos Andrés Pupiales Córdoba <capc3101@gmail.com>
2024-06-18 20:01:09 -0500

    buscar categoria por id

--- src/main/java/com/company/inventory/services/CategoryServiceImp.java
+++ src/main/java/com/company/inventory/services/CategoryServiceImp.java
@@ -1,6 +1,8 @@
 package com.company.inventory.services;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -23,12 +25,39 @@ public class CategoryServiceImp implements ICategoryService {
 	public ResponseEntity<CategoryResponseRest> search() {
 	
 		CategoryResponseRest response = new CategoryResponseRest();
+		
 		try {
 			List<Category> category = (List<Category>) categoryDao.findAll();
 			response.getCategoryResponse().setCategory(category);
 			response.setMetadata("Respuesta OK", "00", "Respuesta exitosa");
 		} catch (Exception e) {
 			response.setMetadata("Respuesta NOK", "-1", "Error al consultar");
+			return new ResponseEntity<CategoryResponseRest>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+		
+		return new ResponseEntity<CategoryResponseRest>(response, HttpStatus.OK);
+	}
+
+	@Override
+	@Transactional(readOnly = true)
+	public ResponseEntity<CategoryResponseRest> searchById(Long id) {
+		
+		CategoryResponseRest response = new CategoryResponseRest();
+		List<Category> list = new ArrayList<>();
+		
+		try {
+			Optional<Category> category = categoryDao.findById(id);
+			if(category.isPresent()) {
+				list.add(category.get());
+				response.getCategoryResponse().setCategory(list);
+				response.setMetadata("Respuesta OK", "00", "Categoria encontrada");
+			} else {
+				response.setMetadata("Respuesta NOK", "-1", "Categoria no encontrada");
+				return new ResponseEntity<CategoryResponseRest>(response, HttpStatus.NOT_FOUND);
+			}
+			
+		} catch (Exception e) {
+			response.setMetadata("Respuesta NOK", "-1", "Error al consultar por id");
 			return new ResponseEntity<CategoryResponseRest>(response, HttpStatus.INTERNAL_SERVER_ERROR);
 		}
 		
